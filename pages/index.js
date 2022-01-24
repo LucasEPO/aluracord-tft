05519d9ca3f8c7467936ec3b9865489dd23272d9
LucasEPO <lucas.epoliveira@gmail.com>
2022-01-24 18:53:48 -0300

    Iniciando projeto

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Boas Vindas</h1>
+            <h2>Discord - Imersão Alura 4</h2>
+        </div>)
+  }
+  
+  export default HomePage
